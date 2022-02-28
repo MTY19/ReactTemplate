f3050acaa6521ce43f678fee3f61ec4662b8e86f
Tahir Yıldırım <tahiryildirim19@gmail.com>
2022-02-28 12:06:33 +0300

    some changes

---
+++ shop-app/src/Navi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Navi</h2>
+      </div>
+    )
+  }
+}
